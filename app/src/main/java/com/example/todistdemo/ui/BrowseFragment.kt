2b61fabc023a40cb1f5f8c002522f07661481067
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2024-11-28 23:41:39 +0200

    add new space

--- app/src/main/java/com/example/todistdemo/ui/BrowseFragment.kt
+++ app/src/main/java/com/example/todistdemo/ui/BrowseFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import com.example.todistdemo.R
 
 class BrowseFragment : Fragment(){
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
